6bdcb2d581081744f6c18344105910492a705b1a
sctech <100468871+sctech-tr@users.noreply.github.com>
2024-10-22 19:43:40 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const http = require('http');
 const { Server } = require('socket.io');
 const cors = require('cors');
 const Filter = require('bad-words');
-const { handleConnection } = require('./socketHandlers');
+const { handleConnection } = require('./socketHandler');
 const { setupMiddleware } = require('./middleware');
 
 const app = express();
